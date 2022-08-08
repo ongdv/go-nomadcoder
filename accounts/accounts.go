01a315dbb2a688af3ca5241e3997f0520cbda5ff
KyeongwooOh <ongdv94@gmail.com>
2022-08-08 19:00:39 +0900

    [UPDATE] #2.1, #2.2
    Function, Method, Try catch in GoLang

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,36 @@
 package accounts
 
+import "errors"
+
 // Account Struct
 type Account struct {
 	owner   string
 	balance int
 }
 
+var errNoMoney = errors.New("can't withdraw you ar poor")
+
 // NewAccount create Accounts
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit X amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+//
+func (a Account) Balance() int {
+	return a.balance
+}
+
+//
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ongdv/learngo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("OngDV")
-	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance())
 }
