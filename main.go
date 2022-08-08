322c720989496b18f1b48de3c1ce54c4eeb53340
KyeongwooOh <ongdv94@gmail.com>
2022-08-08 16:12:29 +0900

    [UPDATE] #1.3
    Function part One
    함수 선언과 함수 인자, 리턴, 여러개 인자 받기 등

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // name := "OngDV" // short form Only working on function
 // var name = "OngDV"
+
+// func multiply(a int, b int) int {
+// 	return a * b
+// }
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
 	// const
@@ -13,7 +26,13 @@ func main() {
 	// name = "ongdv94" // is not working
 
 	// variable
-	var name string = "OngDV" // name :="OngDV"
-	name = "ongdv94"
-	fmt.Println(name)
+	// name := "OngDV" //var name string = "OngDV"
+	// name = "ongdv94"
+
+	// fmt.Println(multiply(2, 2))
+	// totalLength, upperName := lenAndUpper("ongdv")
+	// totalLength, _ := lenAndUpper("ongdv") // _ is ignore value
+	// fmt.Println(totalLength, upperName)
+	repeatMe("ongdv", "kyeongwoo", "oh", "doubleO")
+
 }
