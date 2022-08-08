960266abf92b94c05687014877474a495f8c0d54
KyeongwooOh <ongdv94@gmail.com>
2022-08-08 17:46:58 +0900

    [UPDATE] #1.8
    Pointer

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
-
+	a := 2
+	b := &a
+	a = 10
+	*b = 30
+	fmt.Println(a, *b)
 }
