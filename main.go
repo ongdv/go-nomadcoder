44f1de46567671066501c396f21323e85c02b63c
KyeongwooOh <ongdv94@gmail.com>
2022-08-09 15:05:50 +0900

    [UPDATE] #3.7
    FAST URL Checker
    고 루틴 적용

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 
 var errRequestFailed = errors.New("request failed")
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
 }
 
 func main() {
 	results := make(map[string]string)
-	c := make(chan result)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -31,14 +31,22 @@ func main() {
 	for _, url := range urls {
 		go hitURL(url, c)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
 }
 
-func hitURL(url string, c chan<- result) {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 }
