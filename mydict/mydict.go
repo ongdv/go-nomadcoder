5a2d70b01da515e2fb28181b4bdcbb36daf8b94d
KyeongwooOh <ongdv94@gmail.com>
2022-08-09 11:04:36 +0900

    [UPDATE] #2.5
    Add Method
    map의 접근자

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,11 +6,31 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFund = errors.New("not found")
+var errWordExists = errors.New("invalidate data")
 
+//  Find a word
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
 	if exists {
 		return value, nil
 	}
 	return "", errNotFund
+}
+
+// Add a word
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFund:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	// if err == errNotFund {
+	// 	d[word] = def
+	// } else if err == nil {
+	// 	return errWordExists
+	// }
+
+	return nil
 }
