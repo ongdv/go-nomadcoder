0085248b6ddf3a6d16a2620a8f34b9abe709c64b
KyeongwooOh <ongdv94@gmail.com>
2022-08-09 16:29:37 +0900

    [UPDATE] #4.2 #4.3

--- main.go
+++ main.go
@@ -5,23 +5,55 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		// jobs := getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".job_seen_beacon")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
 }
 
 func getPages() int {
@@ -43,6 +75,16 @@ func getPages() int {
 	return pages
 }
 
+func extractJob(card *goquery.Selection) extractedJob {
+	id_path := card.Find(".jcs-JobTitle")
+	id, _ := id_path.Attr("data-jk")
+	title := cleanStrings(id_path.Find("a>span").Text())
+	location := cleanStrings(card.Find(".companyLocation").Text())
+	salary := card.Find(".salary-snippet-container>div").Text()
+	summary := cleanStrings(card.Find(".job-snippet").Text())
+	return extractedJob{id: id, title: title, location: location, salary: salary, summary: summary}
+}
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
@@ -53,4 +95,8 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("request failed with status:", res.StatusCode)
 	}
+}
+
+func cleanStrings(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
