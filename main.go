b0195278dd1daa6d2f31abf9cb3bf3d51c4bb96b
KyeongwooOh <ongdv94@gmail.com>
2022-08-09 15:01:17 +0900

    [UPDATE] #3.6
    URL Checker + Go Routines

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 
 var errRequestFailed = errors.New("request failed")
 
+type result struct {
+	url    string
+	status string
+}
+
 func main() {
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan result)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -23,25 +29,16 @@ func main() {
 	}
 
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
+		go hitURL(url, c)
 	}
 }
 
-func hitURL(url string) error {
+func hitURL(url string, c chan<- result) {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	c <- result{url: url, status: status}
 }
