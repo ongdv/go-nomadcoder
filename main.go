5965765d3de7121ec882da89553960ff3489b942
KyeongwooOh <ongdv94@gmail.com>
2022-08-09 14:18:41 +0900

    [UPDATE] #3.3
    Channel

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 )
 
 func main() {
-	go sexyCount("ongdv")
-	go sexyCount("ddrtocna")
-	time.Sleep(time.Second * 5)
+	c := make(chan bool)
+	people := [2]string{"ongdv", "kyeongwoo"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	// result := <-c
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
